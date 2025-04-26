d569029f7e8a98bb9edc9a0b262211a61409d8c4
alexcorreia <alexcorreia7@gmail.com>
2025-04-26 13:06:01 -0100

    setup inicial

---
+++ src/api/partecipante/controllers/partecipante.ts
@@ -0,0 +1,7 @@
+/**
+ * partecipante controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::partecipante.partecipante');
---
+++ src/api/partecipante/routes/partecipante.ts
@@ -0,0 +1,7 @@
+/**
+ * partecipante router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::partecipante.partecipante');
